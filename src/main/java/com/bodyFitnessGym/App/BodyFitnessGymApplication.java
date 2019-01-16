be7179ff583250ce753671af5438e3bb09c89a48
JuanDiegost <molinajuan149@gmail.com>
2019-01-16 11:05:05 -0500

    .

--- src/main/java/com/bodyFitnessGym/App/BodyFitnessGymApplication.java
+++ src/main/java/com/bodyFitnessGym/App/BodyFitnessGymApplication.java
@@ -2,8 +2,10 @@ package com.bodyFitnessGym.App;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.bodyFitnessGym.repository")
 public class BodyFitnessGymApplication {
 
 	public static void main(String[] args) {
