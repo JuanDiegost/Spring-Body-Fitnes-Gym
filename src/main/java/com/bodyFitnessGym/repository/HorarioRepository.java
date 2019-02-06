2f325b6aab2116f59855f9bda0654309efc980ed
CesarCardozo <srfeudalwow@gmail.com>
2019-02-05 20:57:58 -0500

    horariio

--- src/main/java/com/bodyFitnessGym/repository/HorarioRepository.java
+++ src/main/java/com/bodyFitnessGym/repository/HorarioRepository.java
@@ -10,8 +10,9 @@ import com.bodyFitnessGym.model.entity.Horario;
 
 public interface HorarioRepository extends CrudRepository<Horario, Long> {
 
-	@Query(value = "SELECT h.dia, h.hora_inicio, h.hora_fin, c.descripcion "
-			+ "from horario h join clase_horario_clase ch on h.id_horario=ch.horario_clase_id_horario join clase c on c.id_clase=ch.clase_id_clase "
+	@Query(value = "SELECT h.dia, h.hora_inicio, h.hora_fin, c.descripcion, s.nombre_servicio from "
+			+ "horario h join clase_horario_clase ch on h.id_pregunta=ch.horario_clase_id_pregunta "
+			+ "join clase c on c.id_clase=ch.clase_id_clase join servicio s on c.servicio_id_servicio=s.id_servicio "
 			+ "WHERE h.dia <= (:diaFinal) AND h.dia >= (:diaInicial)", nativeQuery = true)
 	Collection<Object> filterHorario(String diaInicial, String diaFinal);
 
