2375bd1bcbe509646feba9522e56b970c1af644f
CesarCardozo <srfeudalwow@gmail.com>
2019-02-10 22:27:35 -0500

    Update HorarioRepository.java

--- src/main/java/com/bodyFitnessGym/repository/HorarioRepository.java
+++ src/main/java/com/bodyFitnessGym/repository/HorarioRepository.java
@@ -17,7 +17,7 @@ public interface HorarioRepository extends CrudRepository<Horario, Long> {
 			+ "WHERE h.dia <= (:diaFinal) AND h.dia >= (:diaInicial)", nativeQuery = true)
 	Collection<Object> filterHorario(String diaInicial, String diaFinal);
 
-	@Query(value = "SELECT h.hora_inicio, c.descripcion, s.nombre_servicio, e.nombre_entrenador from "
+	@Query(value = "SELECT c.descripcion, s.nombre_servicio, e.nombre_entrenador from "
 			+ "horario h join clase_horario_clase ch on h.id_horario=ch.horario_clase_id_horario "
 			+ "join clase c on c.id_clase=ch.clase_id_clase join servicio s on c.servicio_id_servicio=s.id_servicio "
 			+ "join entrenador e on c.entrendor_dni_entrenador=e.dni_entrenador "
