eaf3877b24a30fce18178518169ab98b3b0d6ba3
Juan Diego Molina <molinajuan149@gmail.com>
2019-02-17 22:11:16 -0500

    Update Reports.java

--- src/main/java/com/bodyFitnessGym/reports/Reports.java
+++ src/main/java/com/bodyFitnessGym/reports/Reports.java
@@ -28,7 +28,7 @@ import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 public class Reports {
 
 	private static final String PDF_REPORTS_DIR = "/src/main/java/com/bodyfitnessGym/reports/";
-	private static final String REPORT_TEMPLATE_FILENAME = PDF_REPORTS_DIR + "progresos.jrxml";
+	private static final String REPORT_TEMPLATE_FILENAME = "https://firebasestorage.googleapis.com/v0/b/body-fitnes-gym.appspot.com/o/progresos.jrxml?alt=media&token=81a199bd-a7f6-4a3a-9856-fcee3a3e375f";
 
 	/**
 	 * Retorna un PDF con los progresos del alumno, el alumno debe tener una lista de progresos.
@@ -41,7 +41,8 @@ public class Reports {
 	 */
 	public static byte[] generarProgresosPDF(Alumno alumno)
 			throws ClassNotFoundException, IOException, ParseException, JRException {
-		JasperReport jasperReport = JasperCompileManager.compileReport(new File(".").getCanonicalPath()+REPORT_TEMPLATE_FILENAME);
+		URL url = new URL(REPORT_TEMPLATE_FILENAME);
+		JasperReport jasperReport = JasperCompileManager.compileReport(url.openStream());
 		Map<String, Object> parameters = new HashMap<String, Object>();
 		List<ProgresoImagen> imagens = alumno.getHistorialProgresoImagen();
 		Collections.sort(imagens, new Comparator<ProgresoImagen>() {
@@ -58,11 +59,10 @@ public class Reports {
 		parameters.put("urlSecondIMG", imagens.get(imagens.size()-1).getUrl());
 		JasperPrint print = JasperFillManager.fillReport(jasperReport, parameters, new JREmptyDataSource());
 		// Make sure the output directory exists
-		String outFilePath =new File(".").getCanonicalPath()+ PDF_REPORTS_DIR + "Reporte-" + alumno.getDniAlumno() + ".pdf";// REPORTS_DIR
+		String outFilePath ="Reporte-" + alumno.getDniAlumno() + ".pdf";// REPORTS_DIR
 
 		// +persona.getDNI()+persona.getPrimerApellido()+"_"+persona.getPrimerNombre()+"_Carnet.pdf";
-		JasperExportManager.exportReportToPdfFile(print, outFilePath);
-		return getPDFAsByte(outFilePath);
+		return JasperExportManager.exportReportToPdf(print);
 	}
 
 	public static byte[] getPDFAsByte(String filePath) throws IOException {
