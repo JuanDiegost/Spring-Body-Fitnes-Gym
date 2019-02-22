e5dc51e291679588f429c97ff54b60bfae6a71ad
juandiegost <molinajuan149@gmail.com>
2019-02-22 12:00:18 -0500

    Reporte movimientos

--- src/main/java/com/bodyFitnessGym/reports/Reports.java
+++ src/main/java/com/bodyFitnessGym/reports/Reports.java
@@ -1,6 +1,5 @@
 package com.bodyFitnessGym.reports;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
@@ -15,6 +14,7 @@ import java.util.Map;
 import org.springframework.expression.ParseException;
 
 import com.bodyFitnessGym.model.entity.Alumno;
+import com.bodyFitnessGym.model.entity.MovimientoCaja;
 import com.bodyFitnessGym.model.entity.ProgresoImagen;
 
 import net.sf.jasperreports.engine.JREmptyDataSource;
@@ -28,11 +28,15 @@ import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 public class Reports {
 
-	private static final String PDF_REPORTS_DIR = "/src/main/java/com/bodyfitnessGym/reports/";
+	private static final String INGRESO_TEXT = "INGRESO";
+	private static final String PDF_REPORTS_DIR = "src/main/java/com/bodyfitnessGym/reports/";
 	private static final String REPORT_TEMPLATE_FILENAME = "https://firebasestorage.googleapis.com/v0/b/body-fitnes-gym.appspot.com/o/progresos.jrxml?alt=media&token=81a199bd-a7f6-4a3a-9856-fcee3a3e375f";
+	private static final String REPORT_TEMPLATE_CONTABILIDAD = "https://firebasestorage.googleapis.com/v0/b/body-fitnes-gym.appspot.com/o/Blank_A4.jrxml?alt=media&token=c56ad74b-01e1-4278-83b8-e57a12e21c72";
 
 	/**
-	 * Retorna un PDF con los progresos del alumno, el alumno debe tener una lista de progresos.
+	 * Retorna un PDF con los progresos del alumno, el alumno debe tener una lista
+	 * de progresos.
+	 * 
 	 * @param alumno El alumno al que se le quiere generar sus progresos
 	 * @return PDF en un vector de bytes
 	 * @throws ClassNotFoundException
@@ -57,18 +61,43 @@ public class Reports {
 		parameters.put("CHART_DATASET", new JRBeanCollectionDataSource(imagens));
 		parameters.put("CHART_DATASET_GRASA", new JRBeanCollectionDataSource(imagens));
 		parameters.put("urlFirtsIMG", imagens.get(0).getUrl());
-		parameters.put("urlSecondIMG", imagens.get(imagens.size()-1).getUrl());
+		parameters.put("urlSecondIMG", imagens.get(imagens.size() - 1).getUrl());
 		JasperPrint print = JasperFillManager.fillReport(jasperReport, parameters, new JREmptyDataSource());
 		// Make sure the output directory exists
-		String outFilePath ="Reporte-" + alumno.getDniAlumno() + ".pdf";// REPORTS_DIR
 
 		// +persona.getDNI()+persona.getPrimerApellido()+"_"+persona.getPrimerNombre()+"_Carnet.pdf";
 		return JasperExportManager.exportReportToPdf(print);
+	}
+
+	public static byte[] generarMovimientoPDF(List<MovimientoCaja> movimientos)
+			throws ClassNotFoundException, IOException, ParseException, JRException {
+		URL url = new URL(REPORT_TEMPLATE_CONTABILIDAD);
+		JasperReport jasperReport = JasperCompileManager.compileReport(url.openStream());
+		Map<String, Object> parameters = new HashMap<String, Object>();
+
+		parameters.put("DatasetTable", new JRBeanCollectionDataSource(movimientos));
+		parameters.put("TOTAL", getTotal(movimientos));
+		JasperPrint print = JasperFillManager.fillReport(jasperReport, parameters, new JREmptyDataSource());
+		// Make sure the output directory exists
+		return JasperExportManager.exportReportToPdf(print);
+
 	}
 
 	public static byte[] getPDFAsByte(String filePath) throws IOException {
 		Path pdfPath = Paths.get(filePath);
 		return Files.readAllBytes(pdfPath);
 	}
-	
+
+	private static double getTotal(List<MovimientoCaja> movimientoCajas) {
+		double count = 0;
+		for (MovimientoCaja movimientoCaja : movimientoCajas) {
+			if (movimientoCaja.getTipo().equalsIgnoreCase(INGRESO_TEXT)) {
+				count += movimientoCaja.getValor();
+			} else {
+				count -= movimientoCaja.getValor();
+			}
+		}
+		return count;
+	}
+
 }
