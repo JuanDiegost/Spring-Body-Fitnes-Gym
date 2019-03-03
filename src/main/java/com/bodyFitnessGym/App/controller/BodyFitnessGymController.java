743cf3d7b38920cfda53318f6ecb06834cb4d782
juandiegost <molinajuan149@gmail.com>
2019-03-03 11:20:32 -0500

    upload files

--- src/main/java/com/bodyFitnessGym/App/controller/BodyFitnessGymController.java
+++ src/main/java/com/bodyFitnessGym/App/controller/BodyFitnessGymController.java
@@ -5,8 +5,8 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import javax.annotation.Resource;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
@@ -15,14 +15,18 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.bodyFitnessGym.model.entity.Administrador;
@@ -117,6 +121,26 @@ public class BodyFitnessGymController {
 			message = "FAIL to upload " + file.getOriginalFilename() + "!";
 			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
 		}
+	}
+	
+
+	@GetMapping("/getallfiles")
+	public ResponseEntity<List<String>> getListFiles(Model model) {
+		List<String> fileNames = files
+				.stream().map(fileName -> MvcUriComponentsBuilder
+						.fromMethodName(BodyFitnessGymController.class, "getFile", fileName).build().toString())
+				.collect(Collectors.toList());
+ 
+		return ResponseEntity.ok().body(fileNames);
+	}
+ 
+	@GetMapping("/files/{filename:.+}")
+	@ResponseBody
+	public ResponseEntity<org.springframework.core.io.Resource> getFile(@PathVariable String filename) {
+		org.springframework.core.io.Resource file = storageService.loadFile(filename);
+		return ResponseEntity.ok()
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+				.body(file);
 	}
 	
 	// ------------logins---------------------------------------//
