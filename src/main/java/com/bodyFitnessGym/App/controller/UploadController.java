4ade3351940fb7a6f25698aab76c18dc6eb29b2b
juandiegost <molinajuan149@gmail.com>
2019-03-03 11:10:24 -0500

    .

--- src/main/java/com/bodyFitnessGym/App/controller/UploadController.java
+++ src/main/java/com/bodyFitnessGym/App/controller/UploadController.java
@@ -8,17 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.bodyFitnessGym.storage.StorageService;
 
@@ -30,9 +31,8 @@ public class UploadController {
  
 	List<String> files = new ArrayList<String>();
  
-	@RequestMapping(value="/postImage", method = RequestMethod.POST,produces = { MediaType.IMAGE_PNG_VALUE })
-	@ResponseBody
-	public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
+	@PostMapping(value="/postImage")
+	public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
 		String message = "";
 		System.out.println("LOAD IMAGE..");
 		try {
