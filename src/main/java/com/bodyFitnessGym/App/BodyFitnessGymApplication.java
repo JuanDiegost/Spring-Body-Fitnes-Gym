b6b0b2f4447e7c7089c6278ed70d62d83b263713
juandiegost <molinajuan149@gmail.com>
2019-03-03 09:35:41 -0500

    imagenes

--- src/main/java/com/bodyFitnessGym/App/BodyFitnessGymApplication.java
+++ src/main/java/com/bodyFitnessGym/App/BodyFitnessGymApplication.java
@@ -1,21 +1,36 @@
 package com.bodyFitnessGym.App;
 
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import com.bodyFitnessGym.model.entity.Alumno;
+import com.bodyFitnessGym.storage.StorageService;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.bodyFitnessGym"})
 @EnableJpaRepositories("com.bodyFitnessGym.repository")
 @EntityScan(basePackageClasses = Alumno.class)
-public class BodyFitnessGymApplication {
+public class BodyFitnessGymApplication implements CommandLineRunner {
 
+	@Resource
+	StorageService storageService;
+	
 	public static void main(String[] args) {
 		// System.setProperty("server.port","7324");
 		SpringApplication.run(BodyFitnessGymApplication.class, args);
+	}
 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BodyFitnessGymApplication.class);
+	}
+	
+	@Override
+	public void run(String... arg) throws Exception {
+		storageService.init();
 	}
 
 }
